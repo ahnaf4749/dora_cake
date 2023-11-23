db38285cc04c7f17c7ef71664ad510fb4ff228fc
ahnaf4749 <ahnafsagor12@gmail.com>
2023-11-24 01:25:58 +0600

    animation added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,45 @@ import Roadmap from "./pages/Roadmap/Roadmap"
 import Telegram from "./pages/Telegram/Telegram"
 import Tokenomics from "./pages/Tokenomics/Tokenomics"
 import Whytoken from "./pages/WhyToken/Whytoken"
+import { cubicBezier, motion } from "framer-motion";
+import './App.css'
 
 function App() {
-
+  const transition = {
+    duration: 1.4,
+    ease: cubicBezier(0.6, 0.01, -0.05, 0.9)
+    // ease: [0.6, 0.01, -0.05, 0.9],
+  }
 
   return (
     <>
       <div className="">
+        <motion.div
+          data-scroll
+          data-scroll-sticky
+          data-scroll-target="#menu-target"
+          animate={{ top: "-100vh", transition: { ...transition, delay: 5 } }}
+          className="preloader"
+        >
+          <div className="preloader__wrapper">
+            <motion.div
+              initial={{ x: -10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1, transition: { ...transition } }}
+              className="preloader__left"
+            >
+              {/* <img src="svg/adeola-logo-left.svg" alt="adeola logo" /> */}
+            </motion.div>
+            <motion.div
+              initial={{ x: 10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1, transition: { ...transition } }}
+              className="preloader__right"
+            >
+              <p className="preloader__text">Welcome to</p>
+              <p className="preloader__text">FATAO<sub>BD</sub> </p>
+              <p className="preloader__text">A life changer plateform</p>
+            </motion.div>
+          </div>
+        </motion.div>
         <Navbar></Navbar>
         <Bennar></Bennar>
         <Telegram></Telegram>
